2369c17d9b27bdc30a444bd9f1e2215ba98558e4
Greg Hughes <greg@fam.computer>
2020-03-25 16:44:18 -0700

    add push token column to admin

--- clinic/admin.py
+++ clinic/admin.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from clinic.models import Doctor, Language, Report, SelfCertificationQuestion
 
 class DoctorAdmin(admin.ModelAdmin):
-	list_display=('name', 'verified', 'get_languages', 'in_session')
+	list_display=('name', 'verified', 'get_languages', 'push_token', 'in_session')
 	list_filter=('verified', 'languages')
 	readonly_fields=('credentials', 'languages', 'utc_offset', 'last_online', 'last_notified', 'self_certification_questions', 'remarks', 'in_session', 'ip_address', 'user_agent', 'fcm_token')
 
 	def get_languages(self, obj):
 		return ", ".join([l.name for l in obj.languages.all()])
 	get_languages.short_description = "Languages"
+
+	def push_token(self, obj):
+		return bool(obj.fcm_token)
 
 admin.site.register(Doctor, DoctorAdmin)
 admin.site.register(Language)
