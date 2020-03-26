53a120eb15149d215bc996dbbeb707afed237d2c
Greg Hughes <greg@fam.computer>
2020-03-26 12:23:12 -0700

    add homepage link to org landing page

--- medicam/settings.py
+++ medicam/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'social_django.middleware.SocialAuthExceptionMiddleware',
+    'django.contrib.sites.middleware.CurrentSiteMiddleware',
 ]
 
 ROOT_URLCONF = 'medicam.urls'
