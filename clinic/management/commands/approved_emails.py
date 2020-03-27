e0da397d149d9f5b6c16499e2363f6e4ee8ed886
Greg Hughes <greg@fam.computer>
2020-03-27 16:20:11 -0700

    approved_emails: also export names

--- clinic/management/commands/approved_emails.py
+++ clinic/management/commands/approved_emails.py
@@ -4,4 +4,5 @@ from django.core.management.base import BaseCommand, CommandError
 class Command(BaseCommand):
 	def handle(self, *args, **kwargs):
 		doctors = Doctor.objects.filter(verified=True, email__isnull=False).exclude(email='')
-		print('\n'.join(list(doctors.values_list('email', flat=True))))
+		rows = [','.join((d.name, d.email)) for d in doctors]
+		print('\n'.join(rows))
