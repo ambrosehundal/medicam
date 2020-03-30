35658f101e15bfcff01a359fd74739a5af91773b
Greg Hughes <greg@fam.computer>
2020-03-29 23:36:32 -0700

    revert making email required

--- clinic/models.py
+++ clinic/models.py
@@ -63,7 +63,7 @@ PROVIDER_TYPE_CHOICES = (
 
 class Doctor(Participant):
 	name = models.CharField(max_length=70, verbose_name=_("full name"))
-	email = models.EmailField()
+	email = models.EmailField(blank=True, null=True)
 	credentials = models.FileField(upload_to=upload_filename, blank=True, null=True)
 	verified = models.BooleanField(default=False, verbose_name=_("approved"), help_text=_("Allows the provider to receive calls. Approving a provider will trigger an email to be sent to them."))
 	verification_problem = models.PositiveIntegerField(default=0, choices=VERIFICATION_PROBLEM_CHOICES, verbose_name=_("reason for non-approval"), help_text=_("Set this if the provider cannot be approved."))
