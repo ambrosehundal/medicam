d9472ce590b070b1506ec15ead4b7351b43ee5f0
Greg Hughes <greg@fam.computer>
2020-03-31 00:31:29 -0700

    reduce video preview size on mobile

--- medicam/settings.py
+++ medicam/settings.py
@@ -164,7 +164,6 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
@@ -216,6 +215,9 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', True)
 EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', False)
 
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 # Firebase
 
@@ -242,5 +244,3 @@ if DEBUG not in ['1', 1, 'true', 'True', True]:
         integrations=[DjangoIntegration()],
         send_default_pii=False
     )
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
